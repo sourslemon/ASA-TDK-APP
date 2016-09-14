48802077471c82d4138b47409691ecd687f5e58c
mickey9910326 <mickey9910326@gmail.com>
2016-09-14 18:29:32 +0800

    AnalogCompratar test update

--- AlalogCompartar/AlalogCompartar.c
+++ AlalogCompartar/AlalogCompartar.c
@@ -1,38 +1,44 @@
 #include "ASA_Lib.h"
 
-int INT4_COUNT = 0;
-int temp = 0;
-
 void AC_init();
+int Count = 0;
+int Count2 = 0;
 
 int main(void)
 {
-	char status = 0, new_status=0;
 	ASA_M128_set();
-	printf("AlalogCompartar test by LiYu\n");
+	printf("AlalogCompartar test by LiYu 16.9.13\n");
+
+	char status = 0, new_status=0;
 	AC_init();
+	printf("%d\n", ACSR);
+	sei();
+
 	while (1) {
 		asm("nop");
 		new_status = (ACSR>>5) && 1;
 		if (new_status != status) {
 			asm("nop");
 			status = new_status;
-			printf("Changed! status = %d\n", status);
+			Count2++;
+			printf("Status Changed! status = %d , Count = %d , Count2 = %d \n", status, Count, Count2);
 		}
-		// printf("status = %d\n", status);
-		// status = (ACSR>>5) && 1;
-		asm("nop");
 	}
 }
 void AC_init() {
 	SFIOR = 1 << ACME;
-	ACSR  = (1<<ACIE) || (1<<ACIS1) || (1<<ACIS0);
+	ACSR  = (0<<ACD)  | (0<<ACBG) | (0<<ACO)   | (0<<ACI)
+		  | (1<<ACIE) | (0<<ACIC) | (1<<ACIS1) | (1<<ACIS0);
+}
+ISR(ANALOG_COMP_vect){
+	asm("nop");
+	Count++;
 }
 /**
  *STEP1.SFIOR (Special Function IO Register) set
  *enable ACME (Analog Comparator Multiplexer Enable)
  *STEP2. ACSR (Analog Comparator Control and Status Register) set
- *
- *
- *
+ *STEP3. set ACI
+ *P.59 no.24 $002E ANALOG COMP Analog Comparator
+ *P.61 ANA_COMP
  */
