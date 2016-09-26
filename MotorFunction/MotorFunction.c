aafc4c222f5e05e3373f963a44bf37a9029d0a79
mickey9910326 <mickey9910326@gmail.com>
2016-09-26 11:10:55 +0800

    MotorFunction update

--- MotorFunction/MotorFunction.c
+++ MotorFunction/MotorFunction.c
@@ -34,40 +34,30 @@ int main(void)
 	ASA_M128_set();
 	printf("Motor test by LiYu 16.9.25\n");
 
-	uint8_t PWM_ASA_ID = 1;
-	unsigned int pwm_data = 500;
-	uint8_t check = 0;
-	uint8_t target_cycle = 0;
-	uint8_t teeth = 23; //原24 減掉鑽孔處偵測為23
-
+	int check = 0;
+	uint16_t pwm_data=0;
 	PORTE_init();
+
+	check = motor_set(RIGHT_MOTOR_ID,0,ENABLE);
+	printf("%d\n", check);
+	check = motor_set(LEFT_MOTOR_ID, 0,ENABLE);
+	printf("%d\n", check);
+	check = motor_set(RIGHT_MOTOR_ID,1,0);
+	printf("%d\n", check);
+	check = motor_set(LEFT_MOTOR_ID, 1,500);
+	printf("%d\n", check);
+
+	// check = ASA_PWM00_set(PWM_1_ASA_ID,SET_START_LSBYTE,0x01,0,1);	//set PWM M1 enable
+	// printf("debug2 = %d,%d,%d,%d\n",PWM_1_ASA_ID,SET_START_LSBYTE,0x01,0,1);
+	// printf("c1=%d\n",check);									//check point1
+	// check = ASA_PWM00_put(PWM_1_ASA_ID,PUT_START_LSBYTE,2,&pwm_data);	//put PWM M1 STOP
+	// printf("debug2 = %d,%d,%d,%d\n",PWM_1_ASA_ID,PUT_START_LSBYTE,2,pwm_data);
+	// printf("c2=%d\n",check);
+
 	INT_set(4,3);
-
-	sei();
-
-	check = ASA_PWM00_set(PWM_ASA_ID,SET_START_LSBYTE,0x01,0,1);	//set PWM M1 enable
-	printf("c1=%d\n",check);										//check point1
-	check = ASA_PWM00_put(PWM_ASA_ID,PUT_START_LSBYTE,2,&pwm_data);	//put PWM M1 STOP
-	printf("c2=%d\n",check);										//check point1
-
-	while (1) {
-		printf("\ninput target_cycle:");
-		scanf("%d",&target_cycle);
-
-		pwm_data = 0;
-		ASA_PWM00_put(PWM_ASA_ID,0,2,&pwm_data);
-
-		COUNT = 0;
-		while( COUNT <= target_cycle * teeth ){
-			//do something here ,otherwise get a bug that no break out while
-			printf(" ");
-		}
-
-		printf("\nEND-------");
-		pwm_data = 500;
-		ASA_PWM00_put(PWM_ASA_ID,0,2,&pwm_data);
-	}
-
+	// while (1) {
+	// 	// printf("%d\n", COUNT);
+	// }
 }
 void INT_set(char INT_num, char mode) {
 	// mode 0:low level trigger
@@ -99,31 +89,34 @@ uint8_t motor_set(uint8_t motor_ID, uint8_t mode, uint16_t data) {
 	uint8_t check = 0;
 	if (motor_ID == 0 || motor_ID == 1 ) {
 		pwm_asa_id  = PWM_1_ASA_ID;
-		pwm_channel = motor_ID;
+		pwm_channel = motor_ID+1;
 	} else if (motor_ID == 2 || motor_ID == 3) {
 		pwm_asa_id  = PWM_2_ASA_ID;
-		pwm_channel = motor_ID-2;
+		pwm_channel = motor_ID-1;
 	} else {
 		return 1;
 	}
+	// TODO: 測量最低驅動波寬比為多少
 
 	// mode 0:開關PWM, 1:正轉, 2:反轉
-
 	switch (mode) {
 		case 0:
-			if (data!=0 || data!=1) { return 3; }
-			check = ASA_PWM00_set(pwm_asa_id, SET_START_LSBYTE, (1<<pwm_channel), pwm_channel, data);
+			if (data!=0 && data!=1) { return 3; }
+			// printf("debug1 = %d,%d,%d,%d,%d\n",pwm_asa_id,SET_START_LSBYTE,(1<<(pwm_channel-1)),pwm_channel-1,data);
+			check = ASA_PWM00_set(pwm_asa_id, SET_START_LSBYTE, (1<<(pwm_channel-1)), pwm_channel-1, data);
 			if (check!=0) { return 10+check; }
 			break;
 		case 1:
 			if (data>500) { return 3; }
-			check = ASA_PWM00_put(pwm_asa_id, (PUT_START_LSBYTE + pwm_channel*2), 2, &data);
+			data = 500-data;
+			// printf("debug2 = %d,%d,%d,%d\n",pwm_asa_id,(PUT_START_LSBYTE + pwm_channel*2 - 2),2,data);
+			check = ASA_PWM00_put(pwm_asa_id, (PUT_START_LSBYTE + pwm_channel*2-2), 2, &data);
 			if (check!=0) { return 10+check; }
 			break;
 		case 2:
 			if (data>500) { return 3; }
 			data = data+500;
-			check = ASA_PWM00_put(pwm_asa_id, (PUT_START_LSBYTE + pwm_channel*2), 2, &data);
+			check = ASA_PWM00_put(pwm_asa_id, (PUT_START_LSBYTE + pwm_channel*2-2), 2, &data);
 			if (check!=0) { return 10+check; }
 			break;
 		default:
@@ -150,7 +143,7 @@ void motor_count_steps(){
 	}
 	motor_set(RIGHT_MOTOR_ID,0,DISABLE);
 	motor_set(LEFT_MOTOR_ID, 0,DISABLE);
-	return 0;
+	return ;
 }
 
 /*--------------------------interrupt function set--------------------------*/
@@ -161,7 +154,7 @@ void TIMER2_OVF_init(){
     TCCR2 |= 1;	// set up timer with prescaler = 2
     TCNT2  = 0;	// initialize counter
     TIMSK |=(1 << TOIE2);// enable overflow interrupt
-    sei();// enable global interrupts
+    // sei();// enable global interrupts
 }
 void TIMER2_OVF_reg (void (*function)(void)){
 	TIMER2_OVF_fun=function;
