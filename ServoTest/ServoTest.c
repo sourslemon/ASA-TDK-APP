2b469d06f168e96c384e87fad10199fb6a3c0066
mickey9910326 <mickey9910326@gmail.com>
2016-10-05 14:17:12 +0800

    ServoTest update : replace Timer1 by Timer3

--- ServoTest/ServoTest.c
+++ ServoTest/ServoTest.c
@@ -1,19 +1,20 @@
 #include "ASA_Lib.h"
 
+#define SERVO_GRIPPING 0
+#define SERVO_ROTATING 1
+
+void TIMER3_init();
+void servo_init();
+void servo_set(uint8_t id,uint8_t target_angle);
+
 void main()
 {
     ASA_M128_set();
-	printf("Motor test by LiYu 16.10.02\n");
+	printf("Servo test by LiYu 16.10.02\n");
 
-    TIMER1_init();
+    TIMER3_init();
+    servo_init();
 
-	DDRB|=(1<<PB5)|(1<<PB6)|(1<<PB7);   //PWM Pins as Out
-
-    OCR1B = 1000;
-    OCR1A = 1000;
-    _delay_ms(2000);
-    OCR1B = 700;
-    OCR1A = 700;
     int id = 0;
     int deg = 0;
 	while(1)
@@ -25,14 +26,24 @@ void main()
         servo_set(id,deg);
 	}
 }
-void TIMER1_init(){
+void TIMER3_init(){
     //Configure TIMER1
-	TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM (enable OCR1A,OCR1B)
-	TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(0<<CS10); //PRESCALER=8 MODE 14(FAST PWM)
+	TCCR3A|=(1<<COM3A1)|(1<<COM3B1)|(1<<COM3C1)|(1<<WGM31);        //NON Inverted PWM (enable OCR1A,OCR1B)
+	TCCR3B|=(1<<WGM33)|(1<<WGM32)|(1<<CS31)|(0<<CS30); //PRESCALER=8 MODE 14(FAST PWM)
 
-	ICR1=27992;  //fPWM=50Hz (Period = 20ms Standard).
+	ICR3=27992;  //fPWM=50Hz (Period = 20ms Standard).
     //PRESCALER=8 ICR1=27992
     //PRESCALER=64 ICR1=3499
+
+    DDRB|=(1<<PE5)|(1<<PE6);   //PWM Pins as Out
+}
+void servo_init() {
+    //以大角度啟動後復位到0度
+    servo_set(SERVO_GRIPPING,30);
+    servo_set(SERVO_ROTATING,30);
+    _delay_ms(500);
+    servo_set(SERVO_GRIPPING,0);
+    servo_set(SERVO_ROTATING,0);
 }
 void servo_set(uint8_t id,uint8_t target_angle){
     // 0.00052643 (ms/per_val)
@@ -47,9 +58,9 @@ void servo_set(uint8_t id,uint8_t target_angle){
     uint16_t target_data = (float)target_angle * rate + 700;
     printf("%d\n", target_data);
     if (id == 0) {
-        OCR1A = target_data;
+        OCR3C = target_data;
     } else if (id == 1) {
-        OCR1B = target_data;
+        OCR3B = target_data;
     } else {
         return 1;
     }
